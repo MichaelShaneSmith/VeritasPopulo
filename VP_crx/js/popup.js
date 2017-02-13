1e2cbde348f97b13323b3f5d6af2a3281dd58b33
M. Smith <animaniac21@gmail.com>
2017-02-13 11:24:09 -0600

    created the settings tabs

--- VP_crx/js/popup.js
+++ VP_crx/js/popup.js
@@ -1,1 +1,12 @@
 //Pure JS code with jQuery implementation
+(function() {
+	'use strict';
+	window['counter'] = 0;
+	var snackbarContainer = document.querySelector('#demo-toast-example');
+	var showToastButton = document.querySelector('#demo-show-toast');
+	showToastButton.addEventListener('click', function() {
+		'use strict';
+		var data = {message: 'Example Message # ' + ++counter};
+		snackbarContainer.MaterialSnackbar.showSnackbar(data);
+	});
+}());
