ff20c2ae113a92cc90cbb3d8d6e9ae85d90bf8ef
M. Smith <animaniac21@gmail.com>
2017-02-13 22:00:01 -0600

    got toasts to work

--- VP_crx/js/popup.js
+++ VP_crx/js/popup.js
@@ -1,6 +1,7 @@
 //Pure JS code with jQuery implementation
 (function() {
 	'use strict';
+
 	window['counter'] = 0;
 	var snackbarContainer = document.querySelector('#demo-toast-example');
 	var showToastButton = document.querySelector('#demo-show-toast');
@@ -10,3 +11,18 @@
 		snackbarContainer.MaterialSnackbar.showSnackbar(data);
 	});
 }());
+
+var close_1 = document.getElementById("c1");
+var close_2 = document.getElementById("c2");
+var close_3 = document.getElementById("c3");
+
+var update = function (){
+	//confirm("Hi");
+	var snackbarContainer = document.querySelector('#demo-toast-example');
+	var data = {message: 'Trusted Websites List Updated'};
+	snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
+
+close_1.onclick = update
+close_2.onclick = update
+close_3.onclick = update
