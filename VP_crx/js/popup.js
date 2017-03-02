b0122401cb31227947c842c811094e41efa0c0ca
M. Smith <animaniac21@gmail.com>
2017-03-02 16:13:03 -0600

    Graph stuff

--- VP_crx/js/popup.js
+++ VP_crx/js/popup.js
@@ -27,3 +27,34 @@ close_1.onclick = update
 close_2.onclick = update
 close_3.onclick = update
 
+////////////////////////////////////////////////////////////////////////////////
+
+// Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Rating');
+        data.addColumn('number', 'Sites');
+        data.addRows([
+          ['Neutral', 3],
+          ['Slight Lean', 1],
+          ['Hyper-Partisan', 1],
+          ['Literal Garbage', 1]
+        ]);
+
+       // Set chart options
+        var options = {'title':'Your Browsing History'};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
